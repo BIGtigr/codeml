0512f72ffa6faafaf8085a56ffbac3f34bc43842
Hans Kammler <coolmacmaniac@gmail.com>
2018-04-14 16:11:46 +0530

    Visualising training data
    Plotting the actual data used to train the model along with the fitted regression line.

--- sds-az/Part 2 - Regression/Section 4 - Simple Linear Regression/SimpleLinearRegression.py
+++ sds-az/Part 2 - Regression/Section 4 - Simple Linear Regression/SimpleLinearRegression.py
@@ -10,9 +10,12 @@ Created on  : Wed Apr 11 23:21:00 2018
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 # from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+
+# %%
 
 np.set_printoptions(threshold=np.nan)
 
@@ -41,9 +44,29 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 """
 
+# %%
+
 # fitting simple linear regression to the training set
 linRegressor = LinearRegression()
 linRegressor.fit(X_train, y_train)
 
 # predicting the test results
 y_pred = linRegressor.predict(X_test)
+
+# %%
+
+y_train_pred = linRegressor.predict(X_train)
+
+# visualising the training set results
+# scatter plot plots the each of the data points individually
+plt.scatter(X_train, y_train, c='red', marker='x', label='Training Data')
+# to plot the regression line for the actual salary cases we can use line plot
+# the x-coordinate will be the actual training data set
+# the y-coordinate will be the predicted vector on the actual training data set
+# we can not use y_pred as y-coordinate as it is based on X_test not X_train
+plt.plot(X_train, y_train_pred, color='blue', label='Model')
+plt.title('Salary vs Experience')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.legend(loc='upper-left')
+plt.show()
